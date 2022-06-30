2003f498655146384853f654c569ca473ff50ed6
aghni <aghniihsn@gmail.com>
2022-06-30 11:45:26 +0700

    change porotfolio content

--- src/routes/Portofolio/index.jsx
+++ src/routes/Portofolio/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { Typography, Layout, Card, Col } from "antd";
+import { Typography, Layout, Card, Col, Row } from "antd";
 
 const { Content } = Layout;
 const { Meta } = Card;
@@ -13,19 +13,23 @@ function portofolio() {
         <Title italic className="home-title">
           Portofolio
         </Title>
-        <div className="project">
-          <Col offset={9} lg={8} md={6}>
-            <Card
-              hoverable
-              style={{
-                width: 400,
-              }}
-              cover={<img alt="my project" src="https://aghniihsn.github.io/profile/img/project-hr.png" />}
-            >
-              <Meta title="Clodeo Reminder" description="clodeo reminder" />
+        <Row gutter={[48, 24]}>
+          <Col span={8}>
+            <Card hoverable cover={<img src="https://aghniihsn.github.io/profile/img/agenda.png" alt="img" />}>
+              <Meta title="Clodeo Reminder" description="make agenda list" />
             </Card>
           </Col>
-        </div>
+          <Col span={8}>
+            <Card hoverable cover={<img src="https://aghniihsn.github.io/profile/img/user.png" alt="img" />}>
+              <Meta title="Clodeo Reminder" description="make user list" />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card hoverable cover={<img src="https://aghniihsn.github.io/profile/img/create.png" alt="img" />}>
+              <Meta title="Clodeo Reminder" description="make form create user" />
+            </Card>
+          </Col>
+        </Row>
       </Content>
     </Layout>
   );
