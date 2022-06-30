05c003fd0721a55ff86dc4782e0e4db9b968aef6
aghni <aghniihsn@gmail.com>
2022-06-30 11:51:24 +0700

    coba lagi

--- src/routes/Portofolio/index.jsx
+++ src/routes/Portofolio/index.jsx
@@ -14,17 +14,17 @@ function portofolio() {
           Portofolio
         </Title>
         <Row gutter={[48, 24]}>
-          <Col span={8}>
+          <Col span={8} xs={24}>
             <Card hoverable cover={<img src="https://aghniihsn.github.io/profile/img/agenda.png" alt="img" />}>
               <Meta title="Clodeo Reminder" description="make agenda list" />
             </Card>
           </Col>
-          <Col span={8}>
+          <Col span={8} xs={24}>
             <Card hoverable cover={<img src="https://aghniihsn.github.io/profile/img/user.png" alt="img" />}>
               <Meta title="Clodeo Reminder" description="make user list" />
             </Card>
           </Col>
-          <Col span={8}>
+          <Col span={8} xs={24}>
             <Card hoverable cover={<img src="https://aghniihsn.github.io/profile/img/create.png" alt="img" />}>
               <Meta title="Clodeo Reminder" description="make form create user" />
             </Card>
